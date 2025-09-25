92ba4a4a74b722e107d7031e4aef1047c69cc653
jfung53 <jfung53@pratt.edu>
2025-09-25 17:00:55 -0400

    add reset.css and placeholder chart scripts

---
+++ chart4.js
@@ -0,0 +1,61 @@
+// PLACEHOLDER CHART 1
+
+const canvas4 = document.getElementById('chart4');
+
+// instantiate new chart instance with two arguments:
+// 1. canvas element where chart will be rendered
+// 2. options object
+chart_4 = new Chart (canvas4, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    
+    options: {
+        plugins: {
+            tooltip: true,
+            zoom: {
+                pan: {
+                    enabled: false,
+                    mode: 'xy',
+                },
+                // drag to zoom in
+                zoom: {
+                    drag: {
+                    enabled: true,
+                    borderColor: 'rgb(54, 162, 235)',
+                    borderWidth: 1,
+                    backgroundColor: 'rgba(54, 162, 235, 0.3)',
+                    maintainAspectRatio: false,
+                }
+            }
+        }
+    },
+}
+});
+
+// self made function that just reloads the original chart
+function resetZoom() {
+    chart_4.resetZoom();
+};
---
+++ chart5.js
@@ -0,0 +1,37 @@
+// PLACEHOLDER CHART 3
+
+const canvas5 = document.getElementById('chart5');
+
+// d3's function for reading a csv
+d3.csv('https://s3-us-west-2.amazonaws.com/s.cdpn.io/2814973/atp_wta.csv')
+  .then(makeChart);
+
+//Chart.defaults.font.family = 'IBM Plex Mono';
+
+function makeChart(players) {
+
+    // get csv data and turn it into an array
+    let playerLabels = players.map(function(d) {return d.Name});
+    let weeks = players.map(function(d) {return d.Weeks});
+    // shorthand if statement: if gender is female, colour it red, else blue
+    let playerColors = players.map(function(d) {return d.Gender === 'Female' ? '#F15F36' : '#19A0AA';});
+    
+    // create a chart inside the function
+    let chart = new Chart(canvas5, {
+        type: 'bar',
+        data: {
+            labels: playerLabels,
+            datasets: [
+                {
+                    label: 'Female',
+                    data: weeks,
+                    backgroundColor: playerColors,
+                }
+            ]
+        },
+        options: {
+            // turn bar chart on its side (chart comes from y axis)
+            indexAxis: 'y',
+        }
+    });
+}
